f7da2083831e07a2278861d85fc4e089dc5a6e2a
VighneshHM <vighneshcochin@gmail.com>
2023-08-11 05:06:00 +0530

    forms connected

--- script.js
+++ script.js
@@ -6,36 +6,42 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
-document.getElementById("submit").addEventListener('click', function () {
-    console.log('Button clicked');
-    // window.btoa(document.getElementById("photo").value);
-
-    var data = new FormData();
-    jQuery.each(jQuery('#file')[0].files, function(i, file) {
-        data.append('file-'+i, file);
-    });
-    jQuery.ajax({
-        url: 'https://script.google.com/macros/s/AKfycbxb1_UyqDMOMcrd7oUboIAuxvm4JjJltPKAQUqM0FU31vTag2y4nI6QjMfwIicnPtJI/exec',
-        data: data,
-        cache: false,
-        contentType: false,
-        processData: false,
-        method: 'POST',
-        success: function(){
-            window.location = "index.html";
-            // alert('Registration successful');
-
-        },
-        error: function(textStatus){
-            console.log('Registration failed:', textStatus);
-            window.location = 'index.html';
-            alert( "Regestration failed: " + textStatus );
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("regForm");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the default form submission
+  
+      // Get form data
+      const formData = new FormData(form);
+  
+      // Create and configure an XMLHttpRequest object
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://script.google.com/macros/s/AKfycbxb1_UyqDMOMcrd7oUboIAuxvm4JjJltPKAQUqM0FU31vTag2y4nI6QjMfwIicnPtJI/exec", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+      // Define the callback function for when the request completes
+      xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          // Successful response
+          console.log(xhr.responseText);
+          window.location.href = "index.html";
+          alert('Registration successful');
+        } else {
+          // Error handling
+          console.error("Request failed with status:", xhr.status);
+          alert( "Regestration failed: ", xhr.status);
         }
-    }).done(function() {
-        $(this).addClass('done');
-        window.location = 'index.html';
-        alert('Registration successful');
-        console.log('Registration successful');
-
-    })
-});
+      };
+  
+      // Handle network errors
+      xhr.onerror = function () {
+        console.error("Network error");
+      };
+  
+      // Convert the FormData to a URL-encoded string and send the request
+      const encodedData = new URLSearchParams(formData).toString();
+      xhr.send(encodedData);
+    });
+  });
+  
